d08222fdcfa50c411d509b9780a9a465120dfa9d
onlinefred <67547195+laterinthepast@users.noreply.github.com>
2021-11-26 14:31:20 +0000

    mobile

--- src/app.js
+++ src/app.js
@@ -25,35 +25,42 @@ btnHamburger.addEventListener('click', function () {
     }
 })
 
+/* MODAL IMAGE GALLERY */
 
-
-/* let image = document.querySelector('.imagee');
 let modal = document.querySelector('.modal');
-let imageWrapper = document.querySelector('.modal_wrapper');
-let modalImg = document.querySelector('.modal-img');
+let closeModal = document.querySelector('.close');
+let gallery = document.querySelector('.gallery');
 
 
-if (window.matchMedia("(min-width: 991px)").matches) {
-
-  imageWrapper.addEventListener('click', function (e) {
-    console.log(e)
-
-    let imageClicked = e.path[0];
-    console.log(imageClicked)
-    console.log(modal.style.visability)
-
-    if (imageClicked.classList.contains('modal-img')) {
-        imageClicked.classList.remove('modal-img');
-        modal.style.visibility = "hidden";
-        
-    }
-    else {
-        imageClicked.classList.add('modal-img');
-        modal.style.visibility = "visible";
-        
+gallery.addEventListener('click', function (e) {
+    if (e.target.classList.contains('myImg')) {
+        e.target.classList.add('zoomed');
+        modal.style.display = 'block';
+        closeModal.addEventListener('click', function () {
+            modal.style.display = 'none';
+            e.target.classList.remove('zoomed');
+        })
     }
 
 })
-  
-} */
 
+
+
+/* BACK TO TOP  */
+
+//Get the button:
+mybutton = document.getElementById("myBtn");
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
